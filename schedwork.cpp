9d02514710c49c41ebafdf248a675c59a3da5053
Ye Yint Win <ywin@usc.edu>
2024-04-21 06:10:58 +0000

    final comit

--- schedwork.cpp
+++ schedwork.cpp
@@ -18,12 +18,8 @@ using namespace std;
 // worker ID if that is useful to your implementation.
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
-
-
-// Add prototypes for any helper functions here
-
-
-// Add your implementation of schedule() and other helper functions here
+typedef std::vector<std::vector<Worker_T>> DailySchedule;
+typedef std::vector<std::vector<bool>> AvailabilityMatrix;
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -32,14 +28,75 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
+    if (avail.empty()) {
         return false;
     }
-    sched.clear();
-    // Add your code below
+    else if (avail.size() == sched.size() && sched.back()[dailyNeed - 1] != INVALID_ID) {
+        return true;
+    }
 
+    if (sched.empty() || sched.back()[dailyNeed - 1] != INVALID_ID) {
+        vector<Worker_T> toInsert(dailyNeed + 1, INVALID_ID);
+        toInsert[dailyNeed] = 0;
+        sched.push_back(toInsert);
+    }
 
+    size_t day = sched.size() - 1;
+    vector<Worker_T> currentDaySchedule = sched[day];
+    int nextWorkerIndex = currentDaySchedule[dailyNeed];
+    size_t daySize = avail[day].size();
 
+    for (size_t j = 0; j < daySize; ++j) 
+    {
+        if (!avail[day][j]) continue;
 
+        size_t shiftCount = 0;
+        bool isAlreadyScheduled = false;
+
+        for (size_t currentDay = 0; currentDay <= day; currentDay++) 
+        {
+            for (size_t shiftIndex = 0; shiftIndex < dailyNeed; shiftIndex++) 
+            {
+                if (sched[currentDay][shiftIndex] == j) 
+                {
+                    if (currentDay == day) 
+                    {
+                        isAlreadyScheduled = true;
+                        break;
+                    }
+                    shiftCount++;
+                    if (shiftCount >= maxShifts) 
+                    {
+                        isAlreadyScheduled = true;
+                        break;
+                    }
+                }
+            }
+            if (isAlreadyScheduled) break;
+        }
+
+        if (isAlreadyScheduled) continue;
+
+        currentDaySchedule[nextWorkerIndex] = j;
+        currentDaySchedule[dailyNeed]++;
+
+        sched[day] = currentDaySchedule;
+        if (schedule(avail, dailyNeed, maxShifts, sched)) {
+            if (nextWorkerIndex == 0) {
+                sched[day].pop_back();
+            }
+            return true;
+        }
+
+        currentDaySchedule[nextWorkerIndex] = INVALID_ID;
+        currentDaySchedule[dailyNeed]--;
+        sched[day] = currentDaySchedule;
+    }
+
+    if (sched[day][0] == INVALID_ID) {
+        sched.pop_back();
+    }
+
+    return false;
 }
-
+    
--- wordle.cpp
+++ wordle.cpp
@@ -12,17 +12,62 @@
 using namespace std;
 
 
-// Add prototypes of helper functions here
-
-
-// Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+    unsigned int count = 0;
+    set<string> result;
+    string temp_string = in;
+    for (unsigned int cnt = 0; cnt < in.size(); ++cnt)
+    {
+        if (in.at(cnt) == '-')
+        {
+            count++;
+        }
+    }
+    if (!count)
+    {
+        if (dict.find(temp_string) != dict.end() && floating == "")
+    {
+        result.insert(in);
+    }
+    return result;
+    }
+    
+    if (count < floating.size())
+    {
+        return result;
+    }
+   
+
+ for (unsigned int i = 0; i < temp_string.size(); ++i)
+    {
+        if (temp_string[i] == '-')
+        {
+            for (unsigned int j = 0; j < 26; ++j)
+            {
+                char c = 'a' + j;
+                string pass_on = floating;
+
+                for (unsigned int k = 0; k < floating.size(); ++k)
+                {
+                    if (floating.at(k) == c)
+                    {
+                        pass_on.erase(k, 1);
+                        break;
+                    }
+                }
+                temp_string[i] = c;
+                set<string> produced_strings = wordle(temp_string, pass_on, dict);
+                result.insert(produced_strings.begin(), produced_strings.end());
+            }
+        }
+    }
+
+
+    return result;
+
 
 }
-
-// Define any helper functions here
